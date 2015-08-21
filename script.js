1b0e5de314c9d6cbf6dac67f722e2bca5f319707
IntPtr <lxy.lixiaoyan@gmail.com>
2015-08-21 12:44:20 +0800

    update

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-document.queryAll(".view-box").forEach(function(element) {
-  new IScroll(element, {
-    tap: true,
-    scrollbars: "custom",
-    fadeScrollbars: true,
-    mouseWheel: true,
-    HWCompositing: false
-  });
+(function() {
+  var scrollbar = (function() {
+    var div = document.createElement("div");
+    div.style.position = "absolute";
+    div.style.left = "-9999px";
+    div.style.top = "-9999px";
+    div.style.overflow = "scroll";
+    div.style.width = "100px";
+    div.style.height = "100px";
+    document.body.appendChild(div);
+    var width = div.offsetWidth - div.clientWidth;
+    var height = div.offsetHeight - div.clientHeight;
+    document.body.removeChild(div);
+    return {
+      width: width,
+      height: height
+    };
+  })();
 
-  element.queryAll("a").forEach(function(link) {
-    link.addEventListener("tap", function() {
-      this.click();
-    });
-  });
-});
+  var style = document.createElement("style");
+  document.body.appendChild(style);
+  var index = style.sheet.insertRule(".view-scroll {}", 0);
+  var rule = style.sheet.cssRules[index];
+  rule.style.overflowY = "scroll";
+  rule.style.marginRight = "-" + (scrollbar.width.toString()) + "px";
+})();
